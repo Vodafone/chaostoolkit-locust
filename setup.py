d230dc357fbd56faf86fe5c1c62dd145a5cf1a38
GaborGerencser <gabor.gerencser@vodafone.com>
2022-09-26 12:47:16 +0100

    Updated based on review comments and other chaostoolkit project examples
    Signed-off-by: GaborGerencser <gabor.gerencser@vodafone.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
 import setuptools
+import io
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+packages = setuptools.find_packages(exclude=["unit_tests", "unit_tests.*"])
+
+test_require = []
+with io.open('requirements-dev.txt') as f:
+    test_require = [l.strip() for l in f if not l.startswith('#')]
+
+install_require = []
+with io.open('requirements.txt') as f:
+    install_require = [l.strip() for l in f if not l.startswith('#')]
+
 setuptools.setup(
-    name = "chaostoolkit_locust",
-    version = "0.0.1",
-    author = "Vodafone Systems Team",
-    author_email = "gabor.gerencser@vodafone.com",
-    description = "Locust module for chaostoolkit",
-    classifiers = [
+    name="chaostoolkit_locust",
+    long_description=long_description,
+    license="Apache License Version 2.0",
+    version="0.0.1",
+    author="Vodafone Systems Team",
+    author_email="gabor.gerencser@vodafone.com",
+    description="Locust module for chaostoolkit",
+    classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: No License",
-        "Operating System :: OS Independent" ],
-    packages=setuptools.find_packages(),
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"],
+    packages=packages,
+    install_requires=install_require,
+    tests_require=test_require,
     python_requires=">=3.7",
 )
